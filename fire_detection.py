8d268e7645ab2e88a15b48a117604957adfa4a08
zodwick <115868723+zodwick@users.noreply.github.com>
2023-04-15 15:32:58 +0530

    optimization

--- fire_detection.py
+++ fire_detection.py
@@ -1,30 +1,31 @@
 
 import numpy as np
 import cv2
-#import serial
 import time
 
 
 fire_cascade = cv2.CascadeClassifier('fire_detection.xml')
 #fire_detection.xml file & this code should be in the same folder while running the code as no path is given
 
-# ser1 = serial.Serial('COM14',9600)#change COM port number on which your arduino is connected
 
-cap = cv2.VideoCapture("v5.mp4")
+
+cap = cv2.VideoCapture("v3.mp4")
 while 1:
     ret, img = cap.read()
-    #cv2.imshow('imgorignal',img)
+    # cv2.imshow('imgorignal',img)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    fire = fire_cascade.detectMultiScale(img, 1.2, 5)
+    fire = fire_cascade.detectMultiScale(img, 1.2, 2)
     for (x,y,w,h) in fire:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),4)
+        # roi_gray = gray[y:y+h, x:x+w]
+        # roi_color = img[y:y+h, x:x+w]
+        # edges = cv2.Canny(roi_gray, 100, 200)
+        # roi_color[edges != 0] = (0, 255, 0)
         print ('Fire is detected..!')
         # ser1.write('p')
         time.sleep(0.2)
         
-    cv2.imshow('img',img)
+    cv2.imshow('fire_detector',img)
     # ser1.write('s')
     
     k = cv2.waitKey(30) & 0xff
