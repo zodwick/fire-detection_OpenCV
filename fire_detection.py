8279076a7a675a1464bba4b6b1d17b4e9b8047aa
zodwick <115868723+zodwick@users.noreply.github.com>
2023-04-15 17:53:00 +0530

    casulaities

--- fire_detection.py
+++ fire_detection.py
@@ -9,33 +9,37 @@ fire_cascade = cv2.CascadeClassifier('fire_detection.xml')
 
 
 
-cap = cv2.VideoCapture("v4.mp4")
+cap = cv2.VideoCapture("v5.mp4")
+i=0
+    
 while 1:
     ret, img = cap.read()
     img=cv2.resize(img,(960,540))
     # cv2.imshow('imgorignal',img)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    fire = fire_cascade.detectMultiScale(img, 1.2, 5)
-    
+    fire = fire_cascade.detectMultiScale(img, 1.2, 3)
+    #print(fire)
     
     #scale factor, which determines how much the image size is reduced at each image scale
     #minNeighbors, which determines how many neighbors each candidate rectangle should have to retain it. This is used to reduce false positives.
     
-    i=0
-    
+ 
     for (x,y,w,h) in fire:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),4)
         f_name="fire"+str(i)+".jpg"
         cv2.imwrite(f_name,img)
+        #print(f_name)
 
         # roi_gray = gray[y:y+h, x:x+w]
         # roi_color = img[y:y+h, x:x+w]
         # edges = cv2.Canny(roi_gray, 100, 200)
         # roi_color[edges != 0] = (0, 255, 0)
         print ('Fire is detected..!')
+        print (i)
+        i=i+1
+
         # ser1.write('p')
         time.sleep(0.2)
-        i+=1
         
     cv2.imshow('fire_detector',img)
     # ser1.write('s')
